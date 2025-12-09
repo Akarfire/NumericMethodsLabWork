a7783f808ed942a6fde7e2ea8c705b4435c3661e
K <Akarfire@gmail.com>
2025-12-09 10:15:59 +0300

    [DEV] Better results data structure

--- Source/Results.py
+++ Source/Results.py
@@ -7,8 +7,11 @@ from dataclasses import dataclass, field
 class Results:
     
     # Some results that are going to be sent back to the UI for display
-    statistics : Statistics = field(default_factory=Statistics)
     
-    best_strategy : AlgorithmNames = AlgorithmNames.GREEDY
-    worst_strategy : AlgorithmNames = AlgorithmNames.GREEDY
+    # Final results statistics should be in a sorted order
+    statistics_list : list[list[str, float]] = field(default_factory=list)
+    statistics : dict[str : list[float]] = field(default_factory=dict)
+    
+    best_strategy : str = "GREEDY"
+    worst_strategy : str = "GREEDY"
 
